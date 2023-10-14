2e90c00df55e48524c0e3c4e784f71238e4da90d
arronkiptoo <kiptooaarron@gmail.com>
2023-10-14 18:32:02 +0300

    Completed COdeChallenge

---
+++ README.md
@@ -0,0 +1,79 @@
+ # Project Name
+
+ ### MOVIE TICKET MINI APP
+
+## Author
+### AARON KIPTOO
+
+## Description
+
+Introducing MovieGo, your go-to app for purchasing movie tickets. Browse movies, check showtimes, and book tickets effortlessly. Reserve seats, choose payment options, and enjoy exclusive deals. Download MovieGo now for a seamless movie ticketing experience. Lights, camera, action!
+
+## Table of Content
+* Description
+* Installation Requirement
+* Technology Used 
+* Authors Info
+* Licence
+
+
+## Requirements 
+```
+Laptop
+An access to the Internet
+```
+
+## Installation Proccess
+```
+git clone git@github.com:arronkiptoo/CODE-CHALLENGE-W2.git
+git init
+git add <filename>
+git commit -m "initial commmit"
+git push origin master
+```
+## Technology Used
+```
+HTML
+CSS
+Git
+Javascript
+Github
+Vscode
+Json
+Postman
+```
+## Reference
+
+## Image
+![Screenshot from 2023-10-14 18-27-26](https://github.com/arronkiptoo/CODE-CHALLENGE-W2/assets/144256231/975a5ad5-60c7-4753-a19a-834f0ab7073e)
+
+### known Bugs
+#### There are no known bugs, incase of any please raise an issue
+
+## Authors Info
+
+* Email: kiptooaarron@gmail.com
+* Github: @aaronkiptoo
+
+## License
+MIT License
+
+Copyright (c) 2023 AARON KIPTOO
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ script.js
@@ -0,0 +1,70 @@
+// Global variable to store the currently displayed movie
+let displayedMovie;
+
+// Fetch movie data from the local server
+fetch('http://localhost:3000/films')
+  .then(response => response.json())
+  .then(data => {
+    const films = data;
+
+      // Populate the movie list
+      const filmsList = document.getElementById('films');
+      films.forEach(film => {
+        const li = document.createElement('li');
+        li.classList.add('film', 'item');
+        li.textContent = film.title;
+        li.addEventListener('click', () => displayMovieDetails(film));
+        filmsList.appendChild(li);
+      });
+
+      // Display the details of the first movie
+    if (films.length > 0) {
+        displayMovieDetails(films[0]);
+      }
+    });
+
+    // Function to display movie details
+function displayMovieDetails(movie) {
+    // Update the movie details on the right side
+    const movieTitle = document.getElementById('movie-title');
+    const movieRuntime = document.getElementById('movie-runtime');
+    const movieShowtime = document.getElementById('movie-showtime');
+    const movieTickets = document.getElementById('movie-tickets');
+    const buyTicketBtn = document.getElementById('buy-ticket-btn');
+    const moviePoster = document.getElementById('movie-poster');
+  
+    movieTitle.textContent = movie.title;
+    movieRuntime.textContent = 'Runtime: ' + movie.runtime + ' minutes';
+    movieShowtime.textContent = 'Showtime: ' + movie.showtime;
+    movieTickets.textContent = 'Available Tickets: ' + (movie.capacity - movie.tickets_sold);
+    moviePoster.src = movie.poster;
+  
+    buyTicketBtn.disabled = (movie.capacity - movie.tickets_sold) === 0;
+
+     // Set the displayed movie variable
+  displayedMovie = movie;
+}
+
+// Function to handle the buy ticket button click event
+function handleBuyTicket() {
+    const movieTickets = document.getElementById('movie-tickets');
+    const movie = displayedMovie;
+
+    if (movie.capacity - movie.tickets_sold > 0) {
+        // Reduce the available tickets count
+        movie.tickets_sold += 1;
+
+        // Update the movie details on the right side
+    movieTickets.textContent = 'Available Tickets: ' + (movie.capacity - movie.tickets_sold);
+
+     // Disable the button if all tickets are sold out
+     if (movie.capacity - movie.tickets_sold === 0) {
+        const buyTicketBtn = document.getElementById('buy-ticket-btn');
+        buyTicketBtn.disabled = true;
+      }
+    }
+  }
+
+  // Attach the handleBuyTicket function to the buy ticket button
+const buyTicketBtn = document.getElementById('buy-ticket-btn');
+buyTicketBtn.addEventListener('click', handleBuyTicket);
